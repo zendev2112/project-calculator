727053293da38600df82e2e08dfbbbdb1e4133cc
zendev2112 <zenon.deviagge@gmail.com>
2022-10-25 20:25:41 -0300

    decimal btn fixed

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 //create a calculator object
 
 const calculator = {
-displayValue: '0',
+displayValue: '0', //this property represents the input of the user. It needs to be modified when any of the digits is clicked. 
 firstOperand: null,
 waitingForSecondOperand: false,
 operator: null
 }
+
+function inputDigit(digit){
+const {displayValue} = calculator; //same as const displayValue = calculator.displayValue; es decir, estoy creando la const 'displayValue' y le estoy asignando el valor de la propiedad 'displayValue' del objeto 'calculator'.
+calculator.displayValue = displayValue  === '0' ? digit : displayValue + digit; //si la propiedad 'displayValue' del objeto 'calculator' le asigno el valor de la variable 'const displayValue', y es estrictamente igual a 0, retorna el digit; es decir, el target.value del keys.addEventListener. Si no es estrictamente igual a 0, aa la const 'displayValue' le voy a sumar un 'digit'.
+}
+
+function inputDecimal(dot){
+    if(!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
+
 
 function updateDisplay() {
 const display = document.querySelector('.screen'); //capture with DOM screen class of the input element;
@@ -14,12 +26,13 @@ display.value = calculator.displayValue;
 
 updateDisplay();
 
+
 const keys = document.querySelector('.container'); //event delegation
 keys.addEventListener('click', (event) => {
-    const {target} = event;
+    const {target} = event;//same as const target = event.target
 
     if(!target.matches('button')){
-        return;
+        return;//return para salir de la funcion;
     }
 
     if (target.classList.contains('operator')){
@@ -28,7 +41,8 @@ keys.addEventListener('click', (event) => {
     }
 
     if (target.classList.contains('decimal')){
-        console.log('decimal', target.value);
+        inputDecimal(target.value);
+        updateDisplay();
         return;
     }
 
@@ -37,6 +51,7 @@ keys.addEventListener('click', (event) => {
         return;
     }
 
-    console.log('number', target.value)
+    inputDigit(target.value);
+    updateDisplay();
 
 })
